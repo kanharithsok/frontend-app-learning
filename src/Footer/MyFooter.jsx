42af3dc1b9cf783512702229699ca03146782ad5
ReaksmeyThan <reaksmey.than7up@gmail.com>
2025-05-05 00:48:29 +0700

    translate footer function

--- src/Footer/MyFooter.jsx
+++ src/Footer/MyFooter.jsx
@@ -1,31 +1,29 @@
 import { getConfig } from '@edx/frontend-platform';
 
+import { FormattedMessage } from '@edx/frontend-platform/i18n';
 import { Col, Container, Row } from 'react-bootstrap';
-import { useTranslation } from 'react-i18next';
 import logoWhite from '../assets/logo-white.png';
 import './Footer.scss';
+import './messages';
 
-const MyFooter = () => {
-  const { t } = useTranslation();
+const MyFooter = () => (
+  <div className="d-flex row align-content-center justify-content-center">
+    <footer className="footer-container">
+      {/* Bottom Footer */}
+      <div className="footer-bottom font-red-hat-display">
+        <Container fluid>
+          <Row className="text-left text-white text-md-left">
+            {/* Logo */}
+            <Col xs={12} md={12} lg={2} className="pl-0 d-flex justify-content-center">
+              <img src={logoWhite} alt="Logo" className="logo" />
+            </Col>
 
-  return (
-    <div className="d-flex row align-content-center justify-content-center">
-      <footer className="footer-container">
-        {/* Bottom Footer */}
-        <div className="footer-bottom font-red-hat-display">
-          <Container fluid>
-            <Row className="text-left text-white text-md-left">
-              {/* Logo */}
-              <Col xs={12} md={12} lg={2} className="pl-0 d-flex justify-content-center">
-                <img src={logoWhite} alt="Logo" className="logo" />
-              </Col>
-
-              {/* Copyright */}
-              <Col xs={12} md={12} lg={3} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
-                <p className="small m-0">&copy; 2024 Credit Bureau Cambodia. All rights reserved.</p>
-              </Col>
-              <Col xs={9} md={9} lg={3} className="pb-4 d-flex justify-content-center">
-                {/* <ul className="list-inline m-0">
+            {/* Copyright */}
+            <Col xs={12} md={12} lg={3} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+              <p className="small m-0">&copy; 2024 Credit Bureau Cambodia. All rights reserved.</p>
+            </Col>
+            <Col xs={9} md={9} lg={3} className="pb-4 d-flex justify-content-center">
+              {/* <ul className="list-inline m-0">
                   <li className="list-inline-item mx-2">
                     <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="text-white">
                       <i className="fa fa-facebook fa-2x" />
@@ -47,43 +45,60 @@ const MyFooter = () => {
                     </a>
                   </li>
                 </ul> */}
-              </Col>
-              {/* Links */}
-              <Col xs={12} md={12} lg={4} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
-                <ul className="list-inline m-0">
-                  <li className="list-inline-item mx-2">
-                    <a href={getConfig().LMS_BASE_URL} className="text-white small" data-translate-key="myCourse">
-                      {t('My Course')}
-                    </a>
-                  </li>
-                  |
-                  <li className="list-inline-item mx-2">
-                    <a href={`${getConfig().LMS_BASE_URL}/library`} className="text-white small" data-translate-key="library">
-                      {t('Library')}
-                    </a>
-                  </li>
-                  |
-                  <li className="list-inline-item mx-2">
-                    <a href={`${getConfig().LMS_BASE_URL}/contact`} className="text-white small" data-translate-key="contactUs">
-                      {t('Contact Us')}
-                    </a>
-                  </li>
-                  |
-                  <li className="list-inline-item mx-2">
-                    <a href={getConfig().ACCOUNT_SETTINGS_URL} className="text-white small" data-translate-key="account">
-                      {t('Account')}
-                    </a>
-                  </li>
-                </ul>
-              </Col>
+            </Col>
+            {/* Links */}
+            <Col xs={12} md={12} lg={4} className="mb-4 mb-md-0 d-flex align-items-center justify-content-center">
+              <ul className="list-inline m-0">
+                <li className="list-inline-item mx-2">
+                  <a href={getConfig().LMS_BASE_URL} className="text-white small">
+                    <FormattedMessage
+                      id="learnerVariantDashboard.myCourse"
+                      defaultMessage="My Course"
+                      description="A button used to end the tour of the website"
+                    />
+                  </a>
+                </li>
+                |
+                <li className="list-inline-item mx-2">
+                  <a href={`${getConfig().LMS_BASE_URL}/library`} className="text-white small">
+                    Library
+                    <FormattedMessage
+                      id="learnerVariantDashboard.library"
+                      defaultMessage="Library"
+                      description="The text for the user menu Library navigation link."
+                    />
+                  </a>
+                </li>
+                |
+                <li className="list-inline-item mx-2">
+                  <a href={`${getConfig().LMS_BASE_URL}/contact`} className="text-white small">
+                    Contact Us
+                    <FormattedMessage
+                      id="learnerVariantDashboard.contactUs"
+                      defaultMessage="Contact Us"
+                      description="The text for the user menu Contact Us navigation link."
+                    />
+                  </a>
+                </li>
+                |
+                <li className="list-inline-item mx-2">
+                  <a href={getConfig().ACCOUNT_SETTINGS_URL} className="text-white small">
+                    <FormattedMessage
+                      id="learnerVariantDashboard.account"
+                      defaultMessage="Account"
+                      description="The text for the user menu Account navigation link."
+                    />
+                  </a>
+                </li>
+              </ul>
+            </Col>
 
-            </Row>
-          </Container>
-        </div>
-      </footer>
-    </div>
-  );
-};
+          </Row>
+        </Container>
+      </div>
+    </footer>
+  </div>
+);
 MyFooter.propTypes = {
   // onLanguageSelected: PropTypes.func,
 };
---
+++ src/Footer/messages.js
@@ -0,0 +1,37 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+const messages = defineMessages({
+  home: {
+    id: 'learnerVariantDashboard.home',
+    defaultMessage: 'Home',
+    description: 'The text for the user menu Home navigation link.',
+  },
+  myCourse: {
+    id: 'learnerVariantDashboard.myCourse',
+    defaultMessage: 'My Course',
+    description: 'The text for the user menu My Course navigation link.',
+  },
+  exploreCourse: {
+    id: 'learnerVariantDashboard.exploreCourse',
+    defaultMessage: 'Explore Course',
+    description: 'The text for the user menu Explore Course navigation link.',
+  },
+  library: {
+    id: 'learnerVariantDashboard.library',
+    defaultMessage: 'Library',
+    description: 'The text for the user menu Library navigation link.',
+  },
+  contactUs: {
+    id: 'learnerVariantDashboard.contactUs',
+    defaultMessage: 'Contact Us',
+    description: 'The text for the user menu Contact Us navigation link.',
+  },
+  account: {
+    id: 'learnerVariantDashboard.account',
+    defaultMessage: 'Account',
+    description: 'The text for the user menu Account navigation link.',
+  },
+
+});
+
+export default messages;
